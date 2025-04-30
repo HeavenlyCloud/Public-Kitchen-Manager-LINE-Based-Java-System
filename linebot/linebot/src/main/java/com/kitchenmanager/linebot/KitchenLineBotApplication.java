83e0b9e27a51714df03a2dc8f12cb3e4226c9492
HeavenlyCloud <s24113324@stu.edu.tw>
2025-04-30 17:35:21 +0800

    Initialize Spring Boot project structure with essential files and configurations

---
+++ linebot/linebot/src/main/java/com/kitchenmanager/linebot/KitchenLineBotApplication.java
@@ -0,0 +1,13 @@
+package com.kitchenmanager.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenLineBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenLineBotApplication.class, args);
+	}
+
+}
---
+++ linebot/linebot/src/test/java/com/kitchenmanager/linebot/KitchenLineBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchenmanager.linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenLineBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
